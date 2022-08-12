a524c1dd26dbb6f5547edec6c96742e7ba5216a7
Chetan Chinchulkar <76626599+Chetan-Chinchulkar@users.noreply.github.com>
2022-08-12 10:59:43 +0530

    about section done

--- about.php
+++ about.php
@@ -1,34 +1,47 @@
+<style>
+	.about-section{
+		/* linear gradient background */
+		background: linear-gradient(90deg,#7E0036,#0C4369);
+		
+	}
+	.about-section *{
+		color: white !important;
+		text-align: justify !important;
+	}
+</style>
+
 <section id="about-section" class="about-section section theme-bg-light">
-		<div class="container">
-			<h3 class="section-heading text-center mb-3">The Summit</h3>
-			<div class="section-intro single-col-max mx-auto mb-4">
-				<strong>G Plus Creators Summit</strong>
-				is dedicated to build a platform to bring together celebrated content creators along with amateur and aspiring creators to share ideas and recognize to celebrate passionate influencers; who enrich our communities & our lives. 
-				<br>
-				The purpose of the event is to reward individuals who have pushed boundaries, and above all delivered excellence. 
-				<strong>G Plus Creators Summit</strong> 
-				, 2022 shall be the stalwart and pioneering event in the given segment which will make an unprecedented difference. 
-				
+		<div class="row justify-content-center " style="padding: 0 10vw ;" >
+			<div class="col-md" >
+				<h3 class="section-heading">The Summit</h3>
+				<div class="section-intro single-col-max mx-auto mb-4">
+					<strong>G Plus Creators Summit</strong>
+					is dedicated to build a platform to bring together celebrated content creators along with amateur and aspiring creators to share ideas and recognize to celebrate passionate influencers; who enrich our communities & our lives. 
+					<br>
+					<br>
+					The purpose of the event is to reward individuals who have pushed boundaries, and above all delivered excellence. 
+					<strong>G Plus Creators Summit</strong> 
+					, 2022 shall be the stalwart and pioneering event in the given segment which will make an unprecedented difference. 
+					
+				</div>
 			</div>
-			<div class="benefits-list text-center mb-3" style="max-width: 60%;margin: 0 auto;">
-				<h4 class="text-center mb-4">The Organiser</h4>
-				<ul class="list-unstyled text-start d-inline-block">
-					<li><i class="fas fa-check-circle me-2"></i>
+			<div class="col-md" >
+				<h3 class="section-heading">The Organizers</h3>
+
 						<strong>Creators Summit</strong>	
 						is organized by 
 						<strong>G Plus</strong>
 						, the most popular English weekly infotainment tabloid of Guwahati launched in 2013 that captures the true interest and feel of the city. 
-					</li>
-					<li><i class="fas fa-check-circle me-2"></i>
-						Apart from the <strong>print edition</strong> regular stories, along with analytical insights besides interesting features and snippets from segments like current affairs in the city, opinions, business, fashion & lifestyle, food, health, events, travel, etc. are published in its <strong>digital platforms</strong>	
-					</li>
-					<li><i class="fas fa-check-circle me-2"></i>
-						G Plus also hosts the prestigious <strong>Guwahati Theatre Festival</strong> and <strong>Guwahati Food Awards</strong> every year. 
-					</li>
-					
-				</ul>
+					<br>
+					<br>
 
-				<h4 class="text-center mb-4">What to Expect</h4>
+						Apart from the <strong>print edition</strong> regular stories, along with analytical insights besides interesting features and snippets from segments like current affairs in the city, opinions, business, fashion & lifestyle, food, health, events, travel, etc. are published in its <strong>digital platforms</strong>	
+					<br>
+					<br>
+
+						G Plus also hosts the prestigious <strong>Guwahati Theatre Festival</strong> and <strong>Guwahati Food Awards</strong> every year. 
+
+				<!-- <h4 class="text-center mb-4">What to Expect</h4>
 				<ul class="list-unstyled text-start d-inline-block">
 					<li><i class="fas fa-check-circle me-2"></i>
 						<strong>Interactive sessions </strong>	
@@ -86,22 +99,22 @@
 					
 					
 				</ul>
-			</div><!--//benefits-list-->
+			</div> --><!--//benefits-list-->
 			
 			<!-- <div class="about-cta text-center mb-5"><a class="btn btn-secondary btn-lg  mb-5" href="https://themes.3rdwavemedia.com/bootstrap-templates/startup/devconf-free-bootstrap-4-conference-template-for-tech-conferences-and-events/" target="_blank">Get Your Ticket Today</a></div> -->
 		</div>
 		
-		<div class="media-block theme-bg-primary py-5">
+		<!-- <div class="media-block theme-bg-primary py-5">
 			<div class="container">
-				<h4 class="text-white text-center mb-3">Awards</h4>
-				<div class="section-intro text-center single-col-max mx-auto text-white mb-5">
+				<h4 class=" text-center mb-3">Awards</h4>
+				<div class="section-intro text-center single-col-max mx-auto  mb-5">
 					Celebrating the contribution of the content creators & social media influencers in the modern era and acknowledging & rewarding them in keeping the industry vibrant.
 					<br>
 					Individuals who have established credibility in a specific industry has access to a huge audience and can influence decision-making of their followers. The event will recognize the contribution of digital content creators & influencers in different categories.
 				</div>
-				<div class="benefits-list text-center mb-3" style="max-width: 60%;margin: 0 auto;">
+				<div class="benefits-list text-center mb-3" >
 
-					<h4 class="text-white text-center mb-3">Award Categories</h4>
+					<h4 class=" text-center mb-3">Award Categories</h4>
 					<ul class="list-unstyled text-start d-inline-block" style="color: white;">
 						<li><i class="fas fa-check-circle me-2"></i>
 							Content Creator of the Year
@@ -156,7 +169,7 @@
 
 				
 			</div>
-		</div>
+		</div> -->
 		<!-- //media-block -->
 	</section><!--//about-section-->
 	
